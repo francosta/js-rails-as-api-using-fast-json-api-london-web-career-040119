e73f748d1a72e8fd57040dfe2eb34e34b0c5d6bc
github username <github email address>
2019-05-19 10:20:04 +0100

    Done.

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -4,9 +4,11 @@ class SightingsController < ApplicationController
     @sightings = Sighting.all
     render json: SightingSerializer.new(@sightings).serializable_hash
   end
-  
+
   def show
     @sighting = Sighting.find(params[:id])
-    render json: SightingSerializer.new(@sighting).serializable_hash
+    options = {}
+    options[:include] = [:bird, :location]
+    render json: SightingSerializer.new(@sightings, options).serializable_hash
   end
 end
