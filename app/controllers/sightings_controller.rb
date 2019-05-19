cea57677c930883f56c27c6c0f13d8d701e83c31
github username <github email address>
2019-05-19 10:26:58 +0100

    Fixe show mthod on Sightings Controller. Done.

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -2,11 +2,15 @@ class SightingsController < ApplicationController
 
   def index
     @sightings = Sighting.all
-    render json: SightingSerializer.new(@sightings, {include: [:bird, :location]}).serializable_hash
+    options = {}
+    options[:include] = [:bird, :location]
+    render json: SightingSerializer.new(@sightings, options).serializable_hash
   end
 
   def show
     @sighting = Sighting.find(params[:id])
-    render json: SightingSerializer.new(@sighting, {include: [:bird, :location]}).serializable_hash
+    options = {}
+    options[:include] = [:bird, :location]
+    render json: SightingSerializer.new(@sighting, options).serializable_hash
   end
 end
